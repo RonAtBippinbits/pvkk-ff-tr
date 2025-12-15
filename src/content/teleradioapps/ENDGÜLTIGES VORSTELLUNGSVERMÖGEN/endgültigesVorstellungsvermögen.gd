ce3ce01bff78001c5a6f31564364f50862e14df5
Ron Kaiser <ron@bippinbits.com>
2025-12-15 13:22:21 +0100

    drafted title screen, imported some basic assets

--- src/content/teleradioapps/ENDGÜLTIGES VORSTELLUNGSVERMÖGEN/endgültigesVorstellungsvermögen.gd
+++ src/content/teleradioapps/ENDGÜLTIGES VORSTELLUNGSVERMÖGEN/endgültigesVorstellungsvermögen.gd
@@ -6,5 +6,23 @@ enum STATES {MENU, OVERWOLD, BATTLE, CUTSCENE}
 
 var state:STATES = STATES.MENU
 
+func _ready():
+	state = STATES.MENU
+	title_stage()
+
 func title_stage():
-	print("a")
+	$Audio/Music/MusicTitleScreen.play()
+	os.input.connect_to(os.input.just_pressed_b2, start_new_game)
+	os.input.connect_to(os.input.just_pressed_b3, exit_game)
+
+func continue_game():
+	print("Continue")
+
+func start_new_game():
+	print("Start Game")
+	$MainMenu.hide()
+	# update menu buttons here
+
+func exit_game():
+	os.quit_app()
+	
